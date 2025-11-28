5ebbb62ce30fccb793b56140ac870ccdc87ccc0f
Naïl DEZZAZ <130630779+Imadecrypte@users.noreply.github.com>
2025-11-28 10:16:04 +0100

    style login.vue

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,10 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="flex h-screen">
     <!-- Section soumettre un projet (1/3 de l'écran) -->
-    <div class="w-1/3 bg-src\assets\bg_img_gray.png flex flex-col justify-center items-center p-8">
-      <h2 class="text-2xl font-bold
+<div
+  class="w-1/3 flex flex-col justify-center items-center p-8 bg-cover bg-center"
+  style="background-image: url('src/assets/bg_img_gray.png');"
+>
+      <h2 class="text-2xl text-white font-bold
        mb-4 text-center">Vous êtes une entreprise :</h2>
-      <p class="mb-6 text-center">Soumettez une proposition de projet que nous étudierons et proposerons aux étudiants</p>
+      <p class="mb-6 text-white text-center">Soumettez une proposition de projet que nous étudierons et proposerons aux étudiants</p>
       <form class="w-full space-y-4">
         <input
           type="text"
@@ -23,7 +27,7 @@
         ></textarea>
         <button
           type="submit"
-          class="w-full bg-[#CCFFBC] text-white p-3 rounded-lg hover:bg-blue-600 transition"
+          class="w-full bg-[#CCFFBC] text-black p-3 rounded-lg hover:bg-[#B8E6A8] transition"
         >
           Soumettre
         </button>
@@ -31,8 +35,9 @@
     </div>
 
     <!-- Section connexion (2/3 de l'écran) -->
-    <div class="w-2/3 bg-gray-50 flex flex-col justify-center items-center p-12">
-      <h2 class="text-3xl font-bold mb-6">Connexion Étudiant / Enseignant</h2>
+    <div class="w-2/3 bg-[#1A1D29] flex flex-col justify-center items-center p-12">
+      <h2 class="text-4xl text-white font-bold mb-12 pb-12">Application de gestion des projets de MMI Montbéliard</h2>
+      <h3 class="text-3xl text-white font-bold mb-6">Vous êtes étudiant ou enseignant ?</h3>
       <form class="w-2/3 space-y-4">
         <input
           type="email"
@@ -46,14 +51,14 @@
         />
         <div class="flex items-center justify-between">
           <label class="flex items-center space-x-2">
-            <input type="checkbox" class="form-checkbox h-4 w-4 text-indigo-500" />
-            <span>Se souvenir de moi</span>
+            <input type="checkbox" class="form-checkbox h-4 w-4 text-[#A3CC94]" />
+            <span class="text-white">Se souvenir de moi</span>
           </label>
-          <a href="#" class="text-indigo-500 hover:underline text-sm">Mot de passe oublié ?</a>
+          <a href="#" class="text-[#A3CC94] hover:underline text-sm">Mot de passe oublié ?</a>
         </div>
         <button
           type="submit"
-          class="w-full bg-[#CCFFBC] text-white p-3 rounded-lg hover:bg-indigo-600 transition"
+          class="w-full bg-[#CCFFBC] text-black p-3 rounded-lg hover:bg-[#B8E6A8] transition"
         >
           Se connecter
         </button>
