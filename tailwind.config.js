c1e5ba4c34e50dded5cf6fed5615b571c0412ed3
Naïl DEZZAZ <130630779+Imadecrypte@users.noreply.github.com>
2025-12-12 10:20:04 +0100

    Component Année

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
+// tailwind.config.js
+
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
-    'gray-bg': "url('src/assets/bg_img_gray.png')",
-  },
+        'gray-bg': "url('src/assets/bg_img_gray.png')",
+      },
+
+      fontFamily: {
+        // Instrument Sans sera la police par défaut pour tout le texte.
+        'sans': ['Instrument Sans', ...defaultTheme.fontFamily.sans],
+      },
     }
   },
   plugins: []
