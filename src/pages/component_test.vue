67bf13f7bb65e43fec7f0e8405e9bce9a25aec5e
Naïl DEZZAZ <130630779+Imadecrypte@users.noreply.github.com>
2025-12-12 10:22:34 +0100

    Ajout component + base font

--- src/pages/component_test.vue
+++ src/pages/component_test.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HeaderComponent from '@/components/HeaderComponent.vue';
+import Annee from '@/components/Annee.vue';
 </script>
 
 <template>
   <div>
     <h1>Test des composants</h1>
     <HeaderComponent/>
+    <Annee/>
   </div>
 </template>
