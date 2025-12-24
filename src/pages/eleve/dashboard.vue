62d2084c29a078fc23fd8f1dc6927a1edb3ce925
AlbanChatelet <chateletalban20@gmail.com>
2025-12-24 13:09:06 +0100

    ajout admin eleve enseignant

---
+++ src/pages/eleve/dashboard.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+</script>
+
+<template>
+  <main class="min-h-screen flex items-center justify-center">
+    <h1 class="text-3xl font-bold">
+      Dashboard Élève
+    </h1>
+  </main>
+</template>
